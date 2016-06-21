c8b8318b02fb8fddbf9b5831db94ddf34dcd09c4
yncxcw <ynjassionchen@gmail.com>
2016-06-21 17:57:06 -0600

    add network for server

---
+++ PyDockerMonitor/containerFlow.py
@@ -0,0 +1,107 @@
+#!/bin/python
+import psutil
+import logging
+
+log=logging.getLogger("RMDocker.ContaienrFlow")
+
+class ContainerConnect:
+
+    def __init__(self,lport,laddr,rport,raddr,state):
+        ##local port
+        self.lport = lport
+        ##local address
+        self.laddr = laddr
+        ##remote port
+        self.rport = rport
+        ##remote address
+        self.raddr = raddr
+        ##sate(LISTEN/ESTABLISHED)
+        self.state = state
+
+    def get_lport(self):
+        return self.lport
+
+    def get_laddr(self):
+        return self.laddr
+
+    def get_rport(self):
+        return self.rport
+
+    def get_raddr(self):
+        return self.raddr
+
+    def get_state(self):
+        return self.state
+
+    @staticmethod
+    def _class_to_dict_(obj):
+        assert(isinstance(obj,ContainerConnect))
+        return{
+                "__name__" :ContainerConnect.__name__,
+                "lport"   :str(obj.lport),
+                "laddr"   :obj.laddr,
+                "rport"   :str(obj.rport),
+                "raddr"   :obj.raddr,
+                "state"   :obj.state  
+              }
+
+    @staticmethod
+    def _dict_to_class_(dic):
+        assert(dic["__name__"]==ContainerConnect.__name__)
+        connect = ContainerConnect(
+                                   laddr = dic["laddr"],
+                                   lport = int(dic["lport"]),
+                                   raddr = dic["raddr"],
+                                   rport = int(dic["rport"])
+                                   state = dic["state"]
+                                  )
+        return connect
+
+
+class ContainerFlow: 
+
+    def __init__(self,pid):
+        ##monitor pid
+        self.pid = pid
+
+    def monitor(self):
+        try:
+            process = psutil.Process(self.pid)
+        except Exception as excep:
+            log.error("getting process error: %s",excep)
+        try:    
+            connections = process.get_connections()
+        except Exception as excep:
+            log.error("getting connection error: %s",excep)
+
+        if connections is None or len(connections) == 0:
+            return None
+
+        containerConnects = []
+        for connect in connections:
+            ##iterate each connect
+            ##str for ip address and int for port
+            if len(connect.laddr) != 0:
+                laddr = connect.laddr[0]
+                lport = connect.laddr[1]
+            else:
+                laddr = ""
+                lport = -1
+            ##can be 0 for listening port
+            if len(connect.raddr) != 0:
+                raddr = connect.raddr[0]
+                rport = connect.raddr[1]
+            else:
+                raddr = ""
+                rport = -1
+
+            status = connect.status
+            containerConnect = ContainerConnect(
+                                               lport = lport
+                                               laddr = laddr
+                                               rport = rport
+                                               raddr = raddr
+                                               )
+            containerConnects.append(containerConnect)
+
+
