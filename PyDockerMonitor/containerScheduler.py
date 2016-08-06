c91d9692a621c5eef044f0fd107408cab1aa7a7e
yncxcw <ynjassionchen@gmail.com>
2016-08-06 16:45:16 -0600

    write slow 4

--- PyDockerMonitor/containerScheduler.py
+++ PyDockerMonitor/containerScheduler.py
@@ -128,12 +128,7 @@ class ContainerScheduler:
         self.hostToCommands[container.getHost()].append(containerCpuResponse)
         ##suspense memory incrementally
         while memory_value > 128:
-            if memory_value > 4*1024:
-                memory_value = int(memory_value-4096)
-            elif memory_value > 1024 and memory_value < 1024*5:
-                memory_value = int(memory_value-1024)
-            else:
-                memory_value = int(memory_value * 0.5)
+            memory_value = int(memory_value * 0.5)
             memory_value_str = str(memory_value)+"m"
             cgroupMemoryKeyValue={
                                  "memory":{
